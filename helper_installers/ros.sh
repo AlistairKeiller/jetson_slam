7b5080d991266d8bd8231b346b735ff461afda45
Alistair Keiller <alistair@keiller.net>
2022-06-17 14:33:49 -0700

    add update

--- helper_installers/ros.sh
+++ helper_installers/ros.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # http://wiki.ros.org/noetic/Installation/Ubuntu
-# wget -qO- https://raw.githubusercontent.com/qboticslabs/ros_install_noetic/master/ros_install_noetic.sh | bash
 echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/ros-latest.list
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+apt-get update
 apt-get -y --no-install-recommends install \
     ros-noetic-desktop-full \
     python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential \
