b157f3c5d7a8613760d92c268896a436ca2a301c
Alistair Keiller <alistair@keiller.net>
2022-06-16 19:21:16 -0700

    update build

--- isaac-build.sh
+++ isaac-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This need to be run on an x86_64 machine ( think it also needs an nvidia graphics card), then the output can be used on a jetson
+# ***IMPORTANT*** This scripts assumes you are running on an x86 machine and are inside the sdk folder of nvidia isaac
 # this is inspired by isaac-sdk/engine/engine/build/deploy.sh
 
 # set config variables
@@ -7,7 +7,6 @@ DEVICE=jetpack46
 APP=//apps/samples/stereo_dummy:stereo_dummy-pkg
 
 # build the bazel package
-cd isaac_sdk/sdk
 ../engine/engine/build/scripts/install_dependencies.sh
 ../engine/engine/build/scripts/prepare_minidump_tools.sh
 bazel build --config $DEVICE $APP --strip=always
