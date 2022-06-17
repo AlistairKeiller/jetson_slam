dbb27b0c3dbf5a9265886707b6182061f6c6d87b
Alistair Keiller <alistair@keiller.net>
2022-06-17 15:32:42 -0700

    temp test

--- vins_fusion_gpu_fork.sh
+++ vins_fusion_gpu_fork.sh
@@ -8,14 +8,14 @@ do
   bash helper_installers/${dep}.sh
 done
 
-# install apt dependencies
-apt-get install -y --no-install-recommends ros-noetic-tf ros-noetic-image-transport
+# # install apt dependencies
+# apt-get install -y --no-install-recommends ros-noetic-tf ros-noetic-image-transport
 
-# build vins_fusion
-source /opt/ros/noetic/setup.bash
-mkdir -p catkin_ws/src
-cd catkin_ws/src
-git clone https://github.com/pjrambo/VINS-Fusion-gpu
-cd ..
-catkin_make
-source ~/catkin_ws/devel/setup.bash
+# # build vins_fusion
+# source /opt/ros/noetic/setup.bash
+# mkdir -p catkin_ws/src
+# cd catkin_ws/src
+# git clone https://github.com/pjrambo/VINS-Fusion-gpu
+# cd ..
+# catkin_make
+# source $(pwd)/devel/setup.bash
