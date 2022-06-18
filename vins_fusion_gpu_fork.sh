33aa41c60d6398af40443ce37c4370618ca16b1e
Alistair Keiller <alistair@keiller.net>
2022-06-17 18:20:55 -0700

    readd compilation

--- vins_fusion_gpu_fork.sh
+++ vins_fusion_gpu_fork.sh
@@ -2,19 +2,19 @@
 # https://github.com/IOdissey/VINS-Fusion-GPU
 
 # build dependencies
-for dep in setup ros opencv_bridge #setup nvidia ros ceres opencv opencv_bridge
+for dep in setup nvidia ros ceres opencv opencv_bridge
 do
   bash helper_installers/${dep}.sh
 done
 
-# # install apt dependencies
-# apt-get install -y --no-install-recommends ros-noetic-tf ros-noetic-image-transport
+# install apt dependencies
+apt-get install -y --no-install-recommends ros-noetic-tf ros-noetic-image-transport
 
-# # build vins_fusion
-# source /opt/ros/noetic/setup.bash
-# mkdir -p catkin_ws/src
-# cd catkin_ws/src
-# git clone https://github.com/pjrambo/VINS-Fusion-gpu
-# cd ..
-# catkin_make
-# source $(pwd)/devel/setup.bash
+# build vins_fusion
+source /opt/ros/noetic/setup.bash
+mkdir -p catkin_ws/src
+cd catkin_ws/src
+git clone https://github.com/pjrambo/VINS-Fusion-gpu
+cd ..
+catkin_make
+source $(pwd)/devel/setup.bash
