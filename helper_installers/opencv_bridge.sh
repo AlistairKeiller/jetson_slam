d0d8143d33e9542dc41fc3676b12234dadf63b8d
Alistair Keiller <alistair@keiller.net>
2022-06-18 14:09:38 -0700

    remove vins_fusion

--- helper_installers/opencv_bridge.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# https://github.com/IOdissey/VINS-Fusion-GPU
-source /opt/ros/noetic/setup.bash
-mkdir -p catkin_ws/src
-cd catkin_ws/src
-git clone https://github.com/ros-perception/vision_opencv
-cd ..
-catkin_make
