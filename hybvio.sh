cdbacf8538127eccca1aebe4841e74a6404ff9e9
Alistair Keiller <alistair@keiller.net>
2022-06-19 11:10:11 -0700

    uncomment parts of hybvio

--- hybvio.sh
+++ hybvio.sh
@@ -12,7 +12,7 @@ libopenblas-dev # installing openblas package becuase building caaues error
 git clone --recursive https://github.com/SpectacularAI/HybVIO
 cd HybVIO/3rdparty/mobile-cv-suite
 CC=clang CXX=clang++ OPENBLAS=OFF ./scripts/build.sh
-# cd ../../
-# ./src/slam/download_orb_vocab.sh
-# CC=clang CXX=clang++ cmake -DBUILD_VISUALIZATIONS=ON -DUSE_SLAM=ON -S . -B build
-# cmake --build build -j 4
+cd ../../
+./src/slam/download_orb_vocab.sh
+CC=clang CXX=clang++ cmake -DBUILD_VISUALIZATIONS=ON -DUSE_SLAM=ON -S . -B build
+cmake --build build -j 4
