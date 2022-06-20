2634d42e48782af004d5fae6428a441b92f94a34
Alistair Keiller <alistair@keiller.net>
2022-06-20 10:19:16 -0700

    comment out melodic install for test

--- helper_installers/ros_melodic.sh
+++ helper_installers/ros_melodic.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# http://wiki.ros.org/noetic/Installation/Ubuntu
+# http://wiki.ros.org/melodic/Installation/Ubuntu
 echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/ros-latest.list
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 apt-get update
 apt-get -y --no-install-recommends install \
     ros-melodic-desktop-full \
-    python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential \
-    python-rosdep
-source /opt/ros/noetic/setup.bash
-rosdep init
-rosdep update
+#     python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential \
+#     python-rosdep
+# source /opt/ros/noetic/setup.bash
+# rosdep init
+# rosdep update
