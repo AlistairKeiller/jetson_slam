781f6046a01b95d44cf40e9c36fa604233426670
Alistair Keiller <alistair@keiller.net>
2022-06-21 16:00:23 -0700

    add curl to ros_melodic

--- helper_installers/ros_melodic.sh
+++ helper_installers/ros_melodic.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # http://wiki.ros.org/melodic/Installation/Ubuntu
 echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/ros-latest.list
+apt-get -y --no-install-recommends install curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 apt-get update
 apt-get -y --no-install-recommends install \
