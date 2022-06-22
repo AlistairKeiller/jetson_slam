d36b8b655191ee8b18e0d2e454d5b8fa4a457e4a
Alistair Keiller <alistair@keiller.net>
2022-06-22 12:18:30 -0700

    build gstam from sourced

--- helper_installers/gstam.sh
+++ helper_installers/gstam.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # https://gtsam.org/get_started/
-add-apt-repository -y ppa:borglab/gtsam-develop
-apt-get install -y --no-install-recommends libgtsam-dev libgtsam-unstable-dev
+apt-get install -y --no-install-recommends libboost-all-dev cmake
+git clone https://github.com/borglab/gtsam
+cd gtsam
+cmake -DGTSAM_BUILD_WITH_MARCH_NATIVE=OFF -DGTSAM_WITH_TBB=OFF -S . -B build
+cmake --build build -j 4
+cmake --install build
