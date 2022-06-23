b36e5e2a4812bd320d88a0dbb7d3202300438c12
Alistair Keiller <alistair@keiller.net>
2022-06-23 15:00:27 -0700

    add comment to libpointmatcher

--- helper_installers/libpointmatcher.sh
+++ helper_installers/libpointmatcher.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # https://github.com/ethz-asl/libpointmatcher
+# This script assumes that libnano.sh has been run
 git clone https://github.com/ethz-asl/libpointmatcher
 cd libpointmatcher
 cmake -S . -B build
