8b5b124446f65f70c9cfd0bdc824097adac923c4
Alistair Keiller <alistair@keiller.net>
2022-06-22 21:45:51 -0700

    remove realsense examples

--- helper_installers/realsense.sh
+++ helper_installers/realsense.sh
@@ -5,6 +5,6 @@ git clone https://github.com/IntelRealSense/librealsense
 cd librealsense
 ./scripts/setup_udev_rules.sh
 export CUDACXX="/usr/local/cuda/bin/nvcc"
-cmake -DCMAKE_BUILD_TYPE=release -DBUILD_WITH_CUDA=true -S . -B build
+cmake -DBUILD_EXAMPLES=false -DCMAKE_BUILD_TYPE=release -DBUILD_WITH_CUDA=true -S . -B build
 cmake --build build -j 4
 cmake --install build
