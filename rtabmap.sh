13d55cbc3c46a51f20f66d26a39297168026bd91
Alistair Keiller <alistair@keiller.net>
2022-06-24 12:38:45 -0700

    replace libtorch with arm binaries

--- rtabmap.sh
+++ rtabmap.sh
@@ -15,10 +15,11 @@ apt-get install -y --no-install-recommends libsqlite3-dev git cmake libproj-dev 
 git clone https://github.com/introlab/rtabmap
 cd rtabmap
 
-wget -O libtorch.zip https://download.pytorch.org/libtorch/nightly/cu102/libtorch-cxx11-abi-shared-with-deps-latest.zip
-unzip libtorch.zip
-rm libtorch.zip
+pip3 install gdown
+gdown https://drive.google.com/uc?id=1izv6kmcnqXk9i7-Ey-vldjC-CGfHOGCl
+sudo tar -xf libtorch-1.10.0-Jetson-aarch64-GPU.tar.gz
+ libtorch-1.10.0-Jetson-aarch64-GPU.tar.gz
 
-CUDACXX=/usr/local/cuda/bin/nvcc cmake -DWITH_TORCH=ON -WITH_CERES=ON -DTorch_DIR=$(pwd)/libtorch/share/cmake/Torch -S . -B build
+CUDACXX=/usr/local/cuda/bin/nvcc cmake -WITH_CERES=ON -DWITH_TORCH=ON -DTorch_DIR=$(pwd)/libtorch/share/cmake/Torch -S . -B build
 cmake --build build -j 4
 cmake --install build
