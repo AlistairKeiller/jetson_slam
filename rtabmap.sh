4b4406cf1d37d0b4c4332869c9b0cb942af44382
Alistair Keiller <alistair@keiller.net>
2022-06-24 12:54:42 -0700

    replace SOURCE_DIR with TORCH_DIR

--- rtabmap.sh
+++ rtabmap.sh
@@ -12,12 +12,12 @@ done
 apt-get install -y --no-install-recommends libsqlite3-dev git cmake libproj-dev libqt5svg5-dev liboctomap-dev
 
 # build rtabmap
-SOURCE_DIR=$(pwd)
+TORCH_DIR=$(pwd)/pytorch/torch/share/cmake/Torch
 git clone https://github.com/introlab/rtabmap
 cd rtabmap
 
 bash ../helper_installers/libtorch.sh
 
-CUDACXX=/usr/local/cuda/bin/nvcc cmake -WITH_CERES=ON -DWITH_TORCH=ON -DTorch_DIR=${SOURCE_DIR}/pytorch/torch/share/cmake/Torch -S . -B build
+CUDACXX=/usr/local/cuda/bin/nvcc cmake -WITH_CERES=ON -DWITH_TORCH=ON -DTorch_DIR=${TORCH_DIR} -S . -B build
 cmake --build build -j 4
 cmake --install build
