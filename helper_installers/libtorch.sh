0ee529b27eebd1ea877154d6ca714196f0777882
Alistair Keiller <alistair@keiller.net>
2022-06-24 13:02:28 -0700

    update libtorch

--- helper_installers/libtorch.sh
+++ helper_installers/libtorch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # https://qengineering.eu/install-pytorch-on-jetson-nano.html
 pip3 install gdown
-gdown https://drive.google.com/uc?id=1izv6kmcnqXk9i7-Ey-vldjC-CGfHOGCl -O - --quiet | tar -xz
+gdown https://drive.google.com/uc?id=1OSWB_Wv7rghpiBI3V9Rvj0ZR6bRcAZsY -O - --quiet | tar -xz
