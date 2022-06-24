a486e1d17a1ea43705fe9622cb7c8481d932e666
Alistair Keiller <alistair@keiller.net>
2022-06-24 12:51:48 -0700

    move libtorch into loop

--- rtabmap.sh
+++ rtabmap.sh
@@ -3,7 +3,7 @@
 set -e
 
 # build dependencies
-for dep in setup nvidia gtsam g2o libpointmatcher ceres realsense pcl opencv
+for dep in setup nvidia gtsam g2o libpointmatcher ceres realsense pcl opencv libtorch
 do
   bash helper_installers/${dep}.sh
 done
@@ -12,11 +12,12 @@ done
 apt-get install -y --no-install-recommends libsqlite3-dev git cmake libproj-dev libqt5svg5-dev liboctomap-dev
 
 # build rtabmap
+SOURCE_DIR=$(pwd)
 git clone https://github.com/introlab/rtabmap
 cd rtabmap
 
 bash ../helper_installers/libtorch.sh
 
-CUDACXX=/usr/local/cuda/bin/nvcc cmake -WITH_CERES=ON -DWITH_TORCH=ON -DTorch_DIR=$(pwd)/pytorch/torch/share/cmake/Torch -S . -B build
+CUDACXX=/usr/local/cuda/bin/nvcc cmake -WITH_CERES=ON -DWITH_TORCH=ON -DTorch_DIR=${SOURCE_DIR}/pytorch/torch/share/cmake/Torch -S . -B build
 cmake --build build -j 4
 cmake --install build
