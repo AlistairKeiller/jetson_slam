826869e34d7eac1ac0f640554bec8492bfc2212d
Alistair Keiller <alistair@keiller.net>
2022-06-24 22:58:23 -0700

    ln -f

--- helper_installers/nvidia.sh
+++ helper_installers/nvidia.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # https://pythops.com/post/create-your-own-image-for-jetson-nano-board
 apt-get install -y --no-install-recommends cuda-toolkit-10-2 libcudnn8 libcudnn8-dev gcc-8 g++-8
-ln -s /usr/bin/gcc-8 /usr/local/cuda/bin/gcc
-ln -s /usr/bin/g++-8 /usr/local/cuda/bin/g++
+ln -sf /usr/bin/gcc-8 /usr/local/cuda/bin/gcc
+ln -sf /usr/bin/g++-8 /usr/local/cuda/bin/g++
