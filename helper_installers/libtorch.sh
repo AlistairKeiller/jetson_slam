69ae37204d8997773f0e5458b1b7bd43551a41d7
Alistair Keiller <alistair@keiller.net>
2022-06-24 22:08:58 -0700

    replace -z

--- helper_installers/libtorch.sh
+++ helper_installers/libtorch.sh
@@ -2,4 +2,4 @@
 # https://qengineering.eu/install-pytorch-on-jetson-nano.html https://github.com/Qengineering/PyTorch-Jetson-Nano
 apt-get install libomp5
 pip3 install gdown
-gdown https://drive.google.com/uc?id=1OSWB_Wv7rghpiBI3V9Rvj0ZR6bRcAZsY -O - --quiet | tar -x
+gdown https://drive.google.com/uc?id=1OSWB_Wv7rghpiBI3V9Rvj0ZR6bRcAZsY -O - --quiet | tar -xz
