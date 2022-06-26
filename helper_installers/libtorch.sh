0c1549c613b6b98b6ce6b6c391168f871c566fe6
Alistair Keiller <alistair@keiller.net>
2022-06-25 22:17:24 -0700

    add -y apt

--- helper_installers/libtorch.sh
+++ helper_installers/libtorch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # https://qengineering.eu/install-pytorch-on-jetson-nano.html https://github.com/Qengineering/PyTorch-Jetson-Nano
-apt-get install libomp5
+apt-get install -y --no-install-recommends libomp5
 pip3 install gdown
 gdown https://drive.google.com/uc?id=1OSWB_Wv7rghpiBI3V9Rvj0ZR6bRcAZsY -O - --quiet | tar -xz
